feaf0e1b851528513366159449c42da85f35bd03
Sâmia Braga <90200730+Samia24@users.noreply.github.com>
2024-04-08 12:31:06 -0300

    Delete sistemaBancario directory

--- sistemaBancario/menuConta.h
+++
@@ -1,9 +0,0 @@
-#ifndef MENU_H
-#define MENU_H
-#include "banco.h"
-
-
-void operacoesConta(Banco *banco);
-
-
-#endif
--- sistemaBancario/titular.c
+++
@@ -1,8 +0,0 @@
-#include "titular.h"
-
-Titular *novoTitular(){
-    Titular *novoTitular = (Titular *) malloc(sizeof(Titular));
-    return novoTitular;
-}
-
-
